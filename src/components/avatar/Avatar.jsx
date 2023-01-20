e829d57b56a6e4ef598fa8f21c2542114d5e66fc
desivp77 <desivp93@gmail.com>
2023-01-20 14:42:44 +0200

    up4
    up4

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -2,41 +2,28 @@ import * as React from "react";
 import styles from "./Avatar.module.scss";
 import classNames from "classnames"; 
 import { styled } from "@mui/material/styles";
-import Badge from "@mui/material/Badge";
-import Avatar from "@mui/material/Avatar";
-import Image from 'next/image';
-
-const SmallAvatar = styled(Avatar)(({ theme }) => ({
-  width: 28,
-  height: 28,
-  marginLeft: '8px'
-}));
 
 
 export default function Avatar1({ url, size = 90, verified = false }) {
   return (
-    <div className={classNames(styles.avatar)} width={size} height={size}>
-        <Avatar 
-          className={classNames(styles.image)}
-          alt="Avatar Pic"
-          src={url}
-          width={size}
-          height={size}
-          sx={{ width: 90, height: 90 }}
-        />
-      <Badge 
-        // prop={verified}
-        overlap="circular"
-        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-        badgeContent={
-          <SmallAvatar 
-          className={classNames(styles.badge)} 
-          alt="Badge" 
-          src="/images/verified.svg" />
-        }>
-      </Badge>
 
 
-    </div>
+      <div className={classNames(styles.avatar)} width={size} height={size}>
+            <img
+                alt="avatar"
+                src={url}
+                width={size}
+                height={size}
+                className={classNames(styles.image)}
+            />
+            <img
+                alt="verified icon"
+                src="../images/verified.svg"
+                className={classNames(styles.badge)}
+                width={size / 4}
+                height={size / 4}
+            />
+        </div>
+ 
   );
 }
