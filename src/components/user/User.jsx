385f64df460c78ccfcf89fd46a7589f998c5dd48
desivp77 <desivp93@gmail.com>
2023-01-22 02:01:05 +0200

    up user 3
    up user 3

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -29,8 +29,8 @@ export default function User({ name = "", info = "", avatar = "", size = 55, ver
          <Avatar url={avatar} size={size} verified={verified}/>
          </div>
          <div className="user_info">
-          <div className="name">{name}</div>
-          <div className="info">{info}</div>
+          <div className={classNames(styles.name)}>{name}</div>
+          <div className={classNames(styles.info)}>{info}</div>
           </div>
       </div>
 
