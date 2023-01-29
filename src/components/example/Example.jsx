43b207c32f64f466593e4949ec86f65b80621068
desivp77 <89302273+desivp77@users.noreply.github.com>
2023-01-29 15:03:35 +0200

    update master

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -3,9 +3,11 @@ import Container from "@mui/material/Container";
 import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
- 
-import Header from "../Header.jsx";
-import Avatar1 from "../avatar/Avatar.jsx";
+import Card from "../card/Card.jsx";
+import Header from "../header/Header.jsx";
+import How from "../how/How.jsx";
+import Trending from "../Trending/Trending.jsx";
+import Footer from "../footer/Footer";
 // import User from "../user/User.jsx";
 
 export default function Copyright() {
@@ -14,20 +16,15 @@ export default function Copyright() {
       <Header/> 
       <Container className={classNames(styles.container)} maxWidth="xl">
         <Paper className={classNames(styles.paper)}>
-          <Link href="/about" color="secondary">
+          <Link href="/card" color="secondary">
             Hi
           </Link>
         </Paper>
       </Container>
-
-      <Avatar1/>
-          {/* <User 
-            name=""
-            info=""
-            avatar=""
-            size="55"
-            verified="false"
-         /> */}
+      <Card />
+<Trending />
+{/* <How /> */}
+<Footer />
     </div>
   );
 }
